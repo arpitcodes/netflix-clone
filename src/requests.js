3b5f3c2e880a358694b7a0c90298f886c1f8da3a
Arpit <computerguy.arpit@outlook.com>
2021-02-12 15:25:40 +0530

    third commit

--- src/requests.js
+++ src/requests.js
@@ -1,12 +1,12 @@
 const API_KEY = "698b08f38b88ea12d6e135fd745a038d";
 
 const requests = {
-    fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTrending : `/trending/all/week?api_key=${API_KEY}`,
+    fetchNetflixOriginals : `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated : `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies : `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies : `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies : `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies : `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries : `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
